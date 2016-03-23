90318c8cdcaa0959109ac27e47f5efa517f0b6a1
Micooz <micooz@hotmail.com>
2016-03-24 00:50:42 +0800

    refactor: extract routers into index/ and story/, move img/ to /src/asserts, fix url error of main.scss

---
+++ server/src/main.js
@@ -0,0 +1,24 @@
+import { Router } from 'director';
+import routes from './routes';
+
+// TODO: better import css
+import './css/main.scss';
+
+const router = Router(routes);
+
+// TODO: extract listeners into module
+// 后退前进按钮
+document.getElementsByClassName('control-button')[0].addEventListener('click', () => {
+    window.history.back();
+});
+
+document.getElementsByClassName('control-button')[1].addEventListener('click', () => {
+    window.history.forward();
+});
+
+// TODO: deprecate global vars
+window.globalData = {};
+
+document.addEventListener('DOMContentLoaded', () => {
+    router.init('/');
+});
---
+++ server/src/story/story.js
@@ -0,0 +1,16 @@
+module.exports = (storyId) => {
+  window.onscroll = null;
+  const xhr = new XMLHttpRequest();
+  let storyData;
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4) {
+      if (xhr.status >= 200 && xhr.status < 300 || xhr.status === 304) {
+        document.body.scrollTop = 0;
+        storyData = JSON.parse(xhr.responseText);
+        document.getElementsByClassName('container')[0].innerHTML = storyData.body;
+      }
+    }
+  };
+  xhr.open('get', 'http://news-at.zhihu.com/api/4/news/' + storyId, true);
+  xhr.send(null);
+};
